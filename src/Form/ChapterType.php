0b90efeca56fe6b9ff674cfcda2db85c7a75999b
ob-ivan <ob-ivan@noreply.users.github.com>
2018-07-22 09:17:26 -0700

    Add submit button

--- src/Form/ChapterType.php
+++ src/Form/ChapterType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ChapterType extends AbstractType
 {
@@ -13,6 +14,7 @@ class ChapterType extends AbstractType
     {
         $builder
             ->add('folder', FileType::class, ['label' => 'ZIP archive'])
+            ->add('save', SubmitType::class)
         ;
     }
 
