893eb49208a45ce48246586621b758ee918d9e62
ob-ivan <ob-ivan@noreply.users.github.com>
2021-08-10 18:47:47 +0200

    Add a confirmation question to action parameters

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,7 @@ console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
 document.querySelectorAll('.chapter-list__action').forEach(action =>
     action.addEventListener('click', event => {
-        if (!window.confirm('Are you sure?')) {
+        if (!window.confirm(action.dataset.confirmation)) {
             event.preventDefault();
             event.stopPropagation();
         }
