98d3f2587b7443daa786ee1979d13708a25cc444
ob-ivan <ob-ivan@noreply.users.github.com>
2023-02-15 17:55:26 +0100

    Extract functions to moving to the previous and next pages

--- assets/ts/_read.ts
+++ assets/ts/_read.ts
@@ -6,17 +6,19 @@ export const ReadHorizontal = () => {
         image.style.left = index < currentIndex ? '-100%' : index > currentIndex ? '100%' : '0';
         image.style.opacity = `${index === currentIndex ? 1 : 0}`;
     });
+    const movePrev = () => {
+        currentIndex = Math.max(currentIndex - 1, 1);
+        setImageStyle();
+    };
+    const moveNext = () => {
+        currentIndex = Math.min(currentIndex + 1, imageList.length);
+        setImageStyle();
+    };
 
     const buttonPrev = document.querySelector('.read__button--prev');
     const buttonNext = document.querySelector('.read__button--next');
-    buttonPrev.addEventListener('click', () => {
-        currentIndex = Math.max(currentIndex - 1, 1);
-        setImageStyle();
-    });
-    buttonNext.addEventListener('click', () => {
-        currentIndex = Math.min(currentIndex + 1, imageList.length);
-        setImageStyle();
-    });
+    buttonPrev.addEventListener('click', movePrev);
+    buttonNext.addEventListener('click', moveNext);
 
     setImageStyle();
 };
