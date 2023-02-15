1b9926eac7620f43059981e8ab9c14aa03d3b0ce
ob-ivan <ob-ivan@noreply.users.github.com>
2023-02-15 17:48:15 +0100

    Split the initializer function in sections

--- assets/ts/_read.ts
+++ assets/ts/_read.ts
@@ -1,6 +1,4 @@
 export const ReadHorizontal = () => {
-    const buttonPrev = document.querySelector('.read__button--prev');
-    const buttonNext = document.querySelector('.read__button--next');
     const imageList = document.querySelectorAll('.read__image--horizontal');
     let currentIndex = 1;
     const setImageStyle = () => imageList.forEach((image: HTMLElement) => {
@@ -8,6 +6,9 @@ export const ReadHorizontal = () => {
         image.style.left = index < currentIndex ? '-100%' : index > currentIndex ? '100%' : '0';
         image.style.opacity = `${index === currentIndex ? 1 : 0}`;
     });
+
+    const buttonPrev = document.querySelector('.read__button--prev');
+    const buttonNext = document.querySelector('.read__button--next');
     buttonPrev.addEventListener('click', () => {
         currentIndex = Math.max(currentIndex - 1, 1);
         setImageStyle();
@@ -16,5 +17,6 @@ export const ReadHorizontal = () => {
         currentIndex = Math.min(currentIndex + 1, imageList.length);
         setImageStyle();
     });
+
     setImageStyle();
 };
