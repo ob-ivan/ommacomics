48c74dd7b0df671b735e96ee9e1b272c2a1832d7
ob-ivan <ob-ivan@noreply.users.github.com>
2023-02-15 16:13:27 +0100

    Initialize carousel image positions

--- assets/js/app.ts
+++ assets/js/app.ts
@@ -25,9 +25,11 @@ document.querySelectorAll('.chapter-list__action').forEach((action: HTMLElement)
 const readContainerHorizontal = document.querySelector('.read__container--horizontal');
 const readImageHorizontalList = document.querySelectorAll('.read__image--horizontal');
 let readImageHorizontalVisibleIndex = 1;
+const setReadImageHorizontalLeft = () => readImageHorizontalList.forEach((image: HTMLElement) => {
+    image.style.left = `${(parseInt(image.dataset.index) - readImageHorizontalVisibleIndex) * 100}%`
+});
 readContainerHorizontal.addEventListener('click', event => {
     ++readImageHorizontalVisibleIndex;
-    readImageHorizontalList.forEach((image: HTMLElement) => {
-        image.style.left = `${(parseInt(image.dataset.index) - readImageHorizontalVisibleIndex) * 100}%`
-    })
+    setReadImageHorizontalLeft();
 });
+setReadImageHorizontalLeft();
